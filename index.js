c80261abc7001bbc863a39fc6f5d10472d47bfb8
alessandrafsales <alessandrasales.tech@gmail.com>
2025-02-17 11:51:20 +0000

    Alterado nome da função e realocado condição

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Escrevendo as classes de um Jogo
 
-let attack = ""
+let typeAttack = ""
 
 class heroes{
     constructor(name,age,type){
@@ -9,28 +9,29 @@ class heroes{
         this.type = type //Guerreiro, Mago, Monge, Ninja
     }
 
-    write(){
-        console.log(`O herói(a) ${this.name}, com o tipo ${this.type} atacou usando ${attack}`)
+    attack(){
+        // Modos de ataque
+        if(hero.type === "Mago"){
+            console.log("Usou magia!")
+            typeAttack = "magia"
+        }else if(hero.type === "Guerreiro"){
+            console.log("Usou espada!")
+            typeAttack = "espada"
+        }else if(hero.type === "Monge"){
+            console.log("Usou artes maciais!")
+            typeAttack = "artes maciais"
+        }else {
+            console.log("Usou shuriken!")
+            typeAttack = "shuriken"
+        }
+
+        console.log(`O herói(a) ${this.name}, com o tipo ${this.type} atacou usando ${typeAttack}`)
+    
     }
 }
 
 // Referente ao herói
-let hero = new heroes("Alessandra", 24, "Ninja")
-
-// Modos de ataque
-if(hero.type === "Mago"){
-    console.log("Usou magia!")
-    attack = "magia"
-}else if(hero.type === "Guerreiro"){
-    console.log("Usou espada!")
-    attack = "espada"
-}else if(hero.type === "Monge"){
-    console.log("Usou artes maciais!")
-    attack = "artes maciais"
-}else {
-    console.log("Usou shuriken!")
-    attack = "shuriken"
-}
+let hero = new heroes("Alessandra", 24, "Guerreiro") // name, age, type = Guerreiro, Mago, Monge, Ninja
 
 // Output
-hero.write()
+hero.attack()
